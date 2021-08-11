95a3c477d3ebd49c2d1f9d51394b61680f05a38b
Arati Kulkarni <arati.kulkarni@phonepe.com>
2021-08-11 08:38:11 +0530

    triplets sum

--- two-pointers/triplet-sum/triplet-sum-smaller-return-list.py
+++ two-pointers/triplet-sum/triplet-sum-smaller-return-list.py
@@ -20,21 +20,21 @@ Space complexity: O(N)
 
 def triplet_with_smaller_sum(arr, target):
     arr.sort()
-    count = 0
+    triplets = []
     for i in range(len(arr)-2):
-        count += search_pair(arr, target, arr[i], i)
-    return count
+        search_pair(arr, target, arr[i], i, triplets)
+    return triplets
 
-def search_pair(arr, target, curr_item, first):
+def search_pair(arr, target, curr_item, first, triplets):
     count = 0
     left, right = first + 1, len(arr) - 1
     while(left<right):
         if arr[left]+arr[right]+curr_item < target:
-            count += right - left
+            for i in range(right, left, -1):
+                triplets.append([curr_item, arr[left], arr[i]])
             left += 1
         else:
             right -= 1
-    return count
 
 
 print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
